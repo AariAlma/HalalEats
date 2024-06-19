44c6cc9bb7d518dc44598030e3d50122165d5350
FatinChowdhury <122334684+FatinChowdhury@users.noreply.github.com>
2024-06-18 22:23:09 -0400

    Update restaurant-search.component.spec.ts

--- src/app/restaurant-search/restaurant-search.component.spec.ts
+++ src/app/restaurant-search/restaurant-search.component.spec.ts
@@ -1,1 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { RestaurantSearchComponent } from './restaurant-search.component';
+
+describe('RestaurantSearchComponent', () => {
+  let component: RestaurantSearchComponent;
+  let fixture: ComponentFixture<RestaurantSearchComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [RestaurantSearchComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(RestaurantSearchComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
